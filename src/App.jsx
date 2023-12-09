51ca5a55337a8a73e390d832a918399248a0b0ab
Mariusz Łotocki <mk.lotocki@gmail.com>
2023-12-09 01:51:37 +0100

    Style scroll bar in index.scss
    Install sass

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Main from "./components/layout/Main";
 import Footer from "./components/layout/Footer";
 
 export default function App() {
-  
+
   return (
     <div>
       <Header/>
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,11 +4,31 @@ import { css } from '@emotion/react';
 import { Typography } from "@mui/material";
 
 const styles = css`
+
   .footer {
-    height: 300px;
+    min-height: 300px;
     background-color: black;
     padding: 30px 10px;
     color: white; 
+    position: relative;
+
+  }
+
+  .sign {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    color: #bbbbbb;
+    font-size: 10px;
+    font-weight: 100;
+    letter-spacing: 1px;
+    position: absolute;
+    bottom: 7px;
+    left: 0;
+
+    span {
+      color: #6ccff7;
+    }
   }
 `
 
@@ -23,6 +43,7 @@ export default function Footer() {
         <Typography variant="subtitle2" align="center" sx={{color: "grey"}}>
           Lorem
         </Typography>
+        <Typography className="sign">ARWcode 2023 &copy; <span>&nbsp;Powered by MUI 5.14.20</span></Typography>
       </div>
     </footer>
   )
